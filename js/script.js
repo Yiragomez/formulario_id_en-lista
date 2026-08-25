e4358e3b3b54483411fd952215d177e58c0c7c5f
YIra Dirley Gomez <gomezhurtadoyiradirley@gmail.com>
2026-08-25 01:20:30 +0000

    id guardados en lista

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//crear el registro de usuario
+function crearUsuario(nombre,apellido,pais,ciudad,tipo_documento,numero_de_identificacion,fecha_de_nacimiento,numero_de_celular,datos_personales){
+
+const usuarioCreado = {
+        id : Date.now(),
+        nombreCompleto :`${nombre} ${apellido}`,
+        LugarNacimiento : {
+            pais: pais,
+            ciudad: ciudad
+        },
+        documento : {
+            tipo : tipo_documento,
+            numero : numero_de_identificacion
+        },
+        fecha : fecha_de_nacimiento,
+        numero : numero_de_celular,
+        datospersonales : datos_personales,
+        fechaRegistro: new Date().toLocaleDateString()
+        
+}
+
+    return usuarioCreado
+};
+
+const formulario = document.querySelector('form');
+
+//captura de informacion
+    
+formulario.addEventListener('submit', function (event) {
+
+    event.preventDefault()
+
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const pais = document.getElementById('pais').value;
+    const ciudad = document.getElementById('ciudad').value;
+    const tipo_documento = document.getElementById('tipo_documento').value;
+    const numero_de_identificacion = document.getElementById('numero_identificacion').value;
+    const fecha_de_nacimiento = document.getElementById('fecha_nacimiento').value;
+    const numero_de_celular = document.getElementById('celular').value;
+    const datos_personales = document.getElementById('politica_datos').value;
+
+    const UsuarioGuardado = crearUsuario(nombre,apellido,pais,ciudad,tipo_documento,numero_de_identificacion,fecha_de_nacimiento,numero_de_celular,datos_personales);
+
+    console.log('Registro Creado');
+
+    const usuarioJSON = JSON.stringify(UsuarioGuardado)
+localStorage.setItem(UsuarioGuardado.id,usuarioJSON);
+})
+
+//GUARDADO
+
+
+
